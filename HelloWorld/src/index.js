fa14e531db21fe98a4c1a3f0addd13ace2f222ee
Runal <bandishtirunal@gmail.com>
2021-05-07 11:17:12 +0530

    first commit

---
+++ HelloWorld/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Hello from './Hello';
+import 'tachyons';
+
+ReactDOM.render(
+  <React.StrictMode>
+    < Hello greeting={'Hello' + 'Friiendsss!!!'} />,
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// reportWebVitals();
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
