c39ded25d672f462eafaacdb05cd84f8f68087ad
Runal <bandishtirunal@gmail.com>
2021-05-17 19:19:43 +0530

    First Commit

---
+++ useState Basic Usecase/src/2-useState-basics.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const UseStateBasics = () => {
+  const [text,setText] = useState('Random Title');
+
+  const handleClick = () => {
+    if(text === 'Random Title'){
+    setText('Hello World!');
+    }
+    else{
+      setText('Random Title');
+    }
+  };
+
+  return (
+  <React.Fragment>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handleClick}>
+      Change Title
+    </button>
+  </React.Fragment>
+  );
+};
+
+export default UseStateBasics;
---
+++ useState Basic Usecase/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/1-useState/setup/2-useState-basics';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
