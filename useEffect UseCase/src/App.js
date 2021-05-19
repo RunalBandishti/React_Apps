363959d41fc76274e53cd3556dd8d81027d4cf4a
Runal <bandishtirunal@gmail.com>
2021-05-19 20:27:57 +0530

    useEffect Property

---
+++ useEffect UseCase/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/2-useEffect/final/3-useEffect-fetch-data';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
