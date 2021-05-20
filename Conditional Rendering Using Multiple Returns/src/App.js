f820da04be407138c7471679c283b5d96106626c
Runal <bandishtirunal@gmail.com>
2021-05-20 22:52:51 +0530

    Conditional Rendering

---
+++ Conditional Rendering Using Multiple Returns/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
