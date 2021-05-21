77f871ec4ae07c3aef8a1178703ef0b85e0cdaf9
Runal <bandishtirunal@gmail.com>
2021-05-21 20:02:53 +0530

    Rendering

---
+++ Conditional Rendering Usecase/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
