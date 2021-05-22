bd9bb8f76e43c951dc737f1bb1ab230f46b8ee80
Runal <bandishtirunal@gmail.com>
2021-05-22 23:42:03 +0530

    first commit

---
+++ React Forms Multiple Inputs/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
