ca389f7681ceddc546eabfa7d818336618efa0d5
Runal <bandishtirunal@gmail.com>
2021-05-22 23:41:35 +0530

    first commit

---
+++ React Forms Controlled Inputs/src/1-controlled-inputs.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+// JS
+// const input = document.getElementById('myText');
+// const inputValue = input.value
+// React
+// value, onChange
+
+const ControlledInputs = () => {
+    const [firstName,setFirstName] = useState('');
+    const [email,setEmail] = useState('');
+    const [people,setPeople] = useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(firstName && email){
+            const person = {id: new Date().getTime().toString(),firstName, email};
+            setPeople((people) => {
+                return [...people,person];
+            
+        });
+        setFirstName('');
+        setEmail('');
+    } else{
+
+        }
+
+    };
+  return (
+  <>
+<article>
+    <form className="form" onSubmit={handleSubmit}>
+
+        <div className="from-control">
+            <label htmlFor="firstName">
+                  Name : 
+            </label>
+              <input 
+              type="text" 
+              id="firstName" 
+              name="firstName" 
+              value={firstName}
+              onChange={(e)=> setFirstName(e.target.value)} //Inline Function
+              />
+        </div>
+
+        <div className="from-control">
+            <label htmlFor="email">
+                  Email : 
+            </label>
+              <input 
+              type="email" 
+              id="email" 
+              name="email" 
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)} //Inline Function
+              />
+        </div>
+
+        <button type="submit">
+              Add Person
+        </button>
+    </form>
+    {
+        people.map((person)=>{
+            const {id,firstName,email} = person;
+            return( 
+            <div className="item" key={id}>
+                <h4>{firstName}</h4>
+                <p>{email}</p>
+            </div>
+            );
+        })
+    };
+</article>
+  </>
+  ); 
+};
+
+export default ControlledInputs;
