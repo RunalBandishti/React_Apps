d94c2b35814d1c79dbda1d63eefec10ec87d8305
Runal <bandishtirunal@gmail.com>
2021-05-25 21:18:29 +0530

    usereducer

---
+++ useReducer/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/6-useReducer/setup/index';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
