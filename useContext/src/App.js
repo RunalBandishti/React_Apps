4807fc393f6a41238e2612e34188c2fa8d11b213
Runal <bandishtirunal@gmail.com>
2021-05-25 21:17:52 +0530

    usecontext

---
+++ useContext/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/8-useContext/setup/1-context-api';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
