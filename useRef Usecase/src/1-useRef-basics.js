8632cdba7d8f8bef826474b58a516ed15d425f8b
Runal <bandishtirunal@gmail.com>
2021-05-26 23:06:29 +0530

    useref

---
+++ useRef Usecase/src/1-useRef-basics.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef } from 'react';
+
+// preserves value
+// DOES NOT trigger re-render
+// target DOM nodes/elements
+
+const UseRefBasics = () => {
+    const refContainer = useRef(null);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+    
+    useEffect(() => {
+        console.log(refContainer.current);
+        refContainer.current.focus();
+    });
+
+  return( 
+  <>
+  <form className="form" onSubmit={handleSubmit}>
+      <div>
+          <input type="text" name="" id="" ref={refContainer}/>
+          <button type="submit">Submit</button>
+      </div>
+  </form>
+    
+  </>
+  );
+};
+
+export default UseRefBasics;
---
+++ useRef Usecase/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/5-useRef/setup/1-useRef-basics';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
