140de01fcc16700dd5769a77c39ced04d13e175b
Runal <bandishtirunal@gmail.com>
2021-05-26 23:07:30 +0530

    proptypes

---
+++ Using PropTypes/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/10-prop-types/setup/index';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
---
+++ Using PropTypes/src/Product.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
+
+
+const Product = ({image,name,price}) => {
+
+    const url = image && image.url;
+  return( 
+  <article className='product'>
+      <img src={url|| defaultImage} alt={name} />
+      <h4>{name}</h4>
+      <p>${price || 3.99}</p>
+  </article>
+  );
+};
+
+Product.propTypes = {
+    image:PropTypes.object.isRequired,
+    name:PropTypes.string.isRequired,
+    price:PropTypes.number.isRequired,  
+};
+
+//Also we can use this too
+// Product.defaultProps = {
+//     name: 'default name',
+//     price:3.99,
+//     image:defaultImage,
+// }
+
+export default Product;
