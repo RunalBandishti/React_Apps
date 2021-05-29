b193750b2abea9c665d81cc9a08e8a15814cdb6c
Runal <bandishtirunal@gmail.com>
2021-05-29 22:32:57 +0530

    hooks

---
+++ Custom Hooks/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  )
+}
+
+export default App
+ 
