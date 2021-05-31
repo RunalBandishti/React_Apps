29bb5b681de0f82ece1c00e27e496ff1abf9ec13
Runal <bandishtirunal@gmail.com>
2021-05-31 20:03:24 +0530

    First commit

---
+++ React Router/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Setup from './tutorial/11-react-router/setup/index';
+function App() {
+	return (
+		<div className='container'>
+			<Setup/>
+		</div>
+  );
+
+}
+
+export default App
